379e0f861c62b1520a87dab9612ea2c93afaa2a1
Irakli Kalmikov <ika@Iraklis-MacBook-Pro.local>
2024-02-08 13:09:48 -0500

    Added homework 4

---
+++ Homework 4/cats.js
@@ -0,0 +1,76 @@
+const cats = [
+    {
+        name: 'Charlie',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Lily',
+        adoptionStatus: 'not-available'
+    },
+    {
+        name: 'Coco',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Oreo',
+        adoptionStatus: 'not-available'
+    },
+    {
+        name: 'Luna',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Milo',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Lola',
+        adoptionStatus: 'not-available'
+    },
+    {
+        name: 'Leo',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Willow',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Bella',
+        adoptionStatus: 'not-available'
+    },
+    {
+        name: 'Max',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Cleo',
+        adoptionStatus: 'available'
+    },
+    {
+        name: 'Lucy',
+        adoptionStatus: 'not-available'
+    },
+    {
+        name: 'Daisy',
+        adoptionStatus: 'available'
+    },
+];
+
+let available_cats = [];
+let sentence = 'The Cats available for adoption are: ';
+
+for (var i = 0; i < cats.length; i++ ){
+    if (cats[i].adoptionStatus === 'available'){
+        available_cats.push(cats[i])
+        if (available_cats.length == 1){
+            sentence += cats[i].name;
+        }
+        else{
+            sentence += ', ' + cats[i].name;
+        }
+    }
+}
+sentence += '.'
+
+console.log(sentence);
